31a35629f2e4c3a6ea0c4f4691783202097eb37e
joshacross <joshuaalexandercross@gmail.com>
2022-07-16 21:29:13 -0400

    creating modal for hero

--- components/home/Hero/index.js
+++ components/home/Hero/index.js
@@ -2,6 +2,7 @@
 import { Fragment } from 'react'
 import { Popover, Transition } from '@headlessui/react'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import Apply from 'components/common/Apply';
 
 const navigation = [
   { name: 'About', href: '/about' },
@@ -132,9 +133,9 @@ export default function Hero() {
                 <div className="mt-3 sm:mt-0 sm:ml-3">
                   <a
                     href="#"
-                    className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+                    className="w-full flex items-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
                   >
-                    Complimentary Rate Quote
+                    <Apply />
                   </a>
                 </div>
               </div>
